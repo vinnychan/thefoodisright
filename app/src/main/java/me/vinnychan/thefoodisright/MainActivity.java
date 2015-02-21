cbcfb28ec06d71957ab9367a2dd48fb21d3acfaa
Vincent Chan <v.chan36@gmail.com>
2015-02-21 01:26:19 -0800

    base layout added

--- app/src/main/java/me/vinnychan/thefoodisright/MainActivity.java
+++ app/src/main/java/me/vinnychan/thefoodisright/MainActivity.java
@@ -20,12 +20,12 @@ public class MainActivity extends ActionBarActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = (ListView) findViewById(R.id.listView);
+        //listView = (ListView) findViewById(R.id.listView);
         itemArrayAdapter = new ItemArrayAdapter(getApplicationContext(), R.layout.item_layout);
 
-        Parcelable state = listView.onSaveInstanceState();
-        listView.setAdapter(itemArrayAdapter);
-        listView.onRestoreInstanceState(state);
+        //Parcelable state = listView.onSaveInstanceState();
+        //listView.setAdapter(itemArrayAdapter);
+        //listView.onRestoreInstanceState(state);
 
         InputStream inputStream = getResources().openRawResource(R.raw.stats);
         CSVFile csvFile = new CSVFile(inputStream);
