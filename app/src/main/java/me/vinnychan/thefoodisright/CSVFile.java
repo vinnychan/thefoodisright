3768080899b7b37af35a15a80384c4bf4fa9d770
nguyenhah <nguyenhasc@gmail.com>
2015-02-21 04:21:31 -0800

    random generated and update score

--- app/src/main/java/me/vinnychan/thefoodisright/CSVFile.java
+++ app/src/main/java/me/vinnychan/thefoodisright/CSVFile.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 
 public class CSVFile {
     InputStream inputStream;
--- app/src/main/java/me/vinnychan/thefoodisright/MainActivity.java
+++ app/src/main/java/me/vinnychan/thefoodisright/MainActivity.java
@@ -1,65 +1,68 @@
 package me.vinnychan.thefoodisright;
 
-import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.InputStream;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private ListView listView;
-    private ItemArrayAdapter itemArrayAdapter;
-    List<String[]> scoreList;
+
+    ArrayList<Foods> scoreList;
+    int s = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //listView = (ListView) findViewById(R.id.listView);
-        //itemArrayAdapter = new ItemArrayAdapter(getApplicationContext(), R.layout.item_layout);
-
-        //Parcelable state = listView.onSaveInstanceState();
-        //listView.setAdapter(itemArrayAdapter);
-        //listView.onRestoreInstanceState(state);
-
         InputStream inputStream = getResources().openRawResource(R.raw.legumes);
         CSVFile csvFile = new CSVFile(inputStream);
         scoreList = csvFile.read();
 
-        //for(String[] scoreData:scoreList ) {
-        //    itemArrayAdapter.add(scoreData);
-        //}
     }
     public void onButtonClick(View v){
         //2 result label - TextView
+        Random ran1 = new Random();
+        Random ran2 = new Random();
         TextView text1 = (TextView)findViewById(R.id.foodText1);
         TextView text2 = (TextView)findViewById(R.id.foodText2);
-
-        String a = scoreList.get(0)[0];
-        String b = scoreList.get(1)[0];
-
-        int num1 = Integer.parseInt(scoreList.get(0)[1]);
-        int num2 = Integer.parseInt(scoreList.get(1)[1]);
+        TextView score = (TextView)findViewById(R.id.scoreText);
+        int x = ran1.nextInt(50);
+        int y = ran2.nextInt(50);
+        String a = scoreList.get(x).getName();
+        String b = scoreList.get(y).getName();
+        int num1 = Integer.parseInt(scoreList.get(x).getCalories());
+        int num2 = Integer.parseInt(scoreList.get(y).getCalories());
 
         if (v.getId() == R.id.foodButton1){
             text1.setText(a);
-
+            text2.setText(b);
+            if(x <= y){
+                s++;
+            }
+            else{
+                s--;
+            }
+            score.setText(s+"");
         }
         if (v.getId() == R.id.foodButton2){
+            text1.setText(a);
             text2.setText(b);
+            if(y <= x){
+                s++;
+            }
+            else{
+                s--;
+            }
+            score.setText(s+"");
         }
-
-
-
     }
 
     @Override
