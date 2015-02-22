acd0ec81a81e75a2ec74ce6113b750708814ef2b
Vincent Chan <v.chan36@gmail.com>
2015-02-21 18:53:18 -0800

    updated splash page

--- app/src/main/java/me/vinnychan/thefoodisright/SplashScreen.java
+++ app/src/main/java/me/vinnychan/thefoodisright/SplashScreen.java
@@ -1,24 +1,34 @@
 package me.vinnychan.thefoodisright;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 /**
  * Created by yves on 21/02/15.
  */
 public class SplashScreen extends ActionBarActivity{
 
+    TextView splashTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+        splashTitle = (TextView) findViewById(R.id.splashTitle);
+
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/BradBunR.ttf");
+        splashTitle.setTypeface(face);
+
+
         Thread startTimer = new Thread(){
             @Override
             public void run() {
                 try {
-                    sleep(5000);
+                    sleep(15000);
                     Intent i = new Intent(SplashScreen.this, MainActivity.class);
                     startActivity(i);
                     finish();
